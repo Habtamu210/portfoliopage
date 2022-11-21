9da2c4edd3f5443b9f6564428fe36ceea9f2d984
Habtamu210 <beleyaha@gmail.com>
2022-11-21 05:43:44 -0800

    fix linter errors

--- script.js
+++ script.js
@@ -7,5 +7,4 @@ hamburger.addEventListener('click', () => {
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
-
 }));
