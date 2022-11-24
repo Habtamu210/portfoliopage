d6276ea8b82a5303dab21940208c601d92c59791
Habtamu210 <beleyaha@gmail.com>
2022-11-24 02:56:48 -0800

    fix linter error

--- validate.js
+++ validate.js
@@ -1,27 +1,14 @@
 const form = document.forms[0];
-
 const email = document.getElementById('email');
-
 const error = document.getElementById('error');
-
 const message = document.querySelector('.error-message');
-
 form.addEventListener('submit', (event) => {
-
   const messageWhileError = [];
-
   if (email.value.match(/[A-Z]/)) {
-
     messageWhileError.push('Email should be writen in small leter.');
-
     error.innerHTML = messageWhileError.join(', ');
-
     message.style.display = 'block';
-
     event.preventDefault();
-
     event.stopPropagation();
-
   }
-
 });
